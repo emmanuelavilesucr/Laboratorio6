d4cf50a97dc5bb825ebc4cc47356015134a31009
Emmanuel <emmanuel_aviles@Ubuntu.myguest.virtualbox.org>
2023-11-30 14:56:29 -0600

    Añadir comentarios sobre el codigo

--- callbacks.py
+++ callbacks.py
@@ -3,17 +3,20 @@ import random
 
 class EventManager:
     def __init__(self):
-        self.subscribers = {}
-
+        self.subscribers = {}  # Inicializa el diccionario
+    
+    # Funcion encargada de añadir un suscriptor de un evento específico.
     def subscribe(self, event_type, callback):
         if event_type not in self.subscribers:
             self.subscribers[event_type] = []
         self.subscribers[event_type].append(callback)
-
+    
+    # Funcion encargada de eliminar un suscriptor de un evento específico.
     def unsubscribe(self, event_type, callback):
         if event_type in self.subscribers and callback in self.subscribers[event_type]:
             self.subscribers[event_type].remove(callback)
 
+    # Funcion encargada de notificar a todos los suscriptores registrados cuando ocurre un evento.
     def notify(self, event_type, data=None):
         if event_type in self.subscribers:
             for callback in self.subscribers[event_type]:
@@ -21,14 +24,19 @@ class EventManager:
 
 class RealTimeDataManager:
     def __init__(self, event_manager):
+        # Inicializa los datos y el gestor de eventos
         self.data = {"temperatura": 25.0, "humedad": 60.0}
         self.event_manager = event_manager
 
+
+    # Funcion encargada de simular la actualización periódica de los datos en segundo plano.
     def start_real_time_updates(self):
         while True:
             time.sleep(3)
             self.generate_real_time_data()
 
+
+    # Funcion encargada de generar y notificar los datos en tiempo real a través del evento.
     def generate_real_time_data(self):
         self.data["temperatura"] += random.uniform(-1.0, 1.0)
         self.data["humedad"] += random.uniform(-2.0, 2.0)
@@ -41,13 +49,14 @@ if __name__ == "__main__":
 
     event_manager = EventManager()
 
+    # Esta funcion define el callback que imprime los datos actualizados al stdout
     def callback(data):
         print(f'Datos en tiempo real actualizados: {data}')
 
-    event_manager.subscribe('datos_actualizados', callback)
-    real_time_data_manager = RealTimeDataManager(event_manager)
+    event_manager.subscribe('datos_actualizados', callback)      # Suscribe el callback al evento datos_actualizados
+    real_time_data_manager = RealTimeDataManager(event_manager)  # Inicializa el gestor de datos en tiempo real
 
-# Actualizaciones en tiempo real en segundo plano
+    # Actualizaciones en tiempo real en segundo plano
     import threading
     update_thread = threading.Thread(target=real_time_data_manager.start_real_time_updates)
     update_thread.start()
@@ -55,5 +64,6 @@ if __name__ == "__main__":
     try:
         while True:
             time.sleep(1)
+    # Captura la interrupción del teclado para terminar el programa
     except KeyboardInterrupt:
         print("\nPrograma terminado.")
